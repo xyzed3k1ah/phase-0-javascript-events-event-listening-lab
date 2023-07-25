29d73639d0abb1857168ed96bdb9f5d8a2c306da
Zedikiah <xyzed3k1ah@gmail.com>
2023-07-25 09:15:11 -0600

    javascript events listening lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+}
+
+
